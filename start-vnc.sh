5a8745c9c3e808363e80c94f25e094a4e4903e1c
sakupapa <otakama@gmail.com>
2026-07-18 14:14:29 +0900

    initial

---
+++ start-vnc.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -eu
+
+: "${VNC_PASSWORD:?VNC_PASSWORD を指定してください}"
+
+mkdir -p "$HOME/.vnc"
+umask 077
+printf '%s\n' "$VNC_PASSWORD" | vncpasswd -f > "$HOME/.vnc/passwd"
+
+cat > "$HOME/.vnc/xstartup" <<'EOF'
+#!/bin/sh
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+exec startxfce4
+EOF
+chmod 700 "$HOME/.vnc/xstartup"
+
+exec tigervncserver :0 \
+  -fg \
+  -localhost no \
+  -rfbport 5900 \
+  -geometry "${VNC_GEOMETRY:-1440x900}" \
+  -depth "${VNC_DEPTH:-24}" \
+  -SecurityTypes VncAuth
